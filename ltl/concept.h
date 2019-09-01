3a2421d8fd83b8b20093beab73153f5135d57c6a
qnope(Antoine MORRIER) <antoine.morrier@gmail.com>
2019-09-01 22:56:51 +0200

    [amorrier] boolification of the concept

--- ltl/concept.h
+++ ltl/concept.h
@@ -7,20 +7,21 @@ template <typename T, bool b> using requires_t = std::enable_if_t<b, T>;
 template <bool b> using requires_void = requires_t<void, b>;
 
 template <typename T>
-constexpr auto IsIterable = decltype(is_iterable(std::declval<T>())){};
-template <typename T>
-constexpr auto IsConst = decltype(is_const(std::declval<T>())){};
+constexpr bool IsIterable = decltype(is_iterable(std::declval<T>()))::value;
 
 template <typename T>
-constexpr auto IsFloatingPoint =
-    decltype(is_floating_point(std::declval<T>())){};
+constexpr bool IsConst = decltype(is_const(std::declval<T>()))::value;
 
 template <typename T>
-constexpr auto IsIntegral = decltype(is_integral(std::declval<T>())){};
+constexpr bool IsFloatingPoint =
+    decltype(is_floating_point(std::declval<T>()))::value;
+
+template <typename T>
+constexpr bool IsIntegral = decltype(is_integral(std::declval<T>()))::value;
 
 template <typename T, typename... Ts>
-constexpr auto IsSame =
-    decltype((true_v && ... && (ltl::type_v<T> == ltl::type_v<Ts>))){};
+constexpr bool IsSame =
+    decltype((true_v && ... && (ltl::type_v<T> == ltl::type_v<Ts>)))::value;
 
 } // namespace ltl
 
