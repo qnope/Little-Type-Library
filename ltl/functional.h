565a959abc34b13faff3705a6ffc7a65c19fac67
qnope(Antoine MORRIER) <antoine.morrier@gmail.com>
2019-10-30 14:26:07 +0100

    [amorrier] Improve const and volatilness for currying

--- ltl/functional.h
+++ ltl/functional.h
@@ -6,6 +6,11 @@ namespace ltl {
 //////////////////////////////// Y_Combinator /////////////////////////////////
 template <typename F> struct fix : F {
   template <typename... Args>
+  constexpr decltype(auto) operator()(Args &&... args) const {
+    return static_cast<const F &>(*this)(*this, FWD(args)...);
+  }
+
+  template <typename... Args>
   constexpr decltype(auto) operator()(Args &&... args) {
     return static_cast<F &>(*this)(*this, FWD(args)...);
   }
@@ -14,10 +19,12 @@ template <typename F> fix(F)->fix<F>;
 
 template <typename F, typename... Args>
 constexpr auto report_call(F f, Args... xs) {
-  return [f = std::move(f), xs = tuple_t{std::move(xs)...}](auto &&... _ys) {
+  return [f = std::move(f),
+          xs = tuple_t{std::move(xs)...}](auto &&... _ys) -> decltype(auto) {
     auto ys = tuple_t<decltype(_ys)...>(FWD(_ys)...);
-    auto args = xs + ys;
-    return args([f](auto &&... args) { return f(FWD(args)...); });
+    auto args = xs + std::move(ys);
+    return args(
+        [f](auto &&... args) -> decltype(auto) { return f(FWD(args)...); });
   };
 }
 
