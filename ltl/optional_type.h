2a4f2f612234b01d34b21dc1d9316fbf761b1f2e
amorrier <antoine.morrier@gmail.com>
2020-03-09 11:57:56 +0100

    Add -> to optional_type

--- ltl/optional_type.h
+++ ltl/optional_type.h
@@ -1,22 +1,30 @@
 #pragma once
 
 #include "traits.h"
+#include "Range/AsPointer.h"
 
 namespace ltl {
 
-template <typename... Ts> struct optional_type;
+template <typename... Ts>
+struct optional_type;
 
-template <typename T> struct optional_type<T> {
+template <typename T>
+struct optional_type<T> {
   constexpr optional_type() = default;
   constexpr optional_type(T) noexcept {}
   static constexpr true_t has_value{};
 
   constexpr auto operator*() const noexcept { return T{}; }
+  constexpr auto operator-> () const noexcept { return AsPointer{T{}}; }
 };
 
-template <typename T> optional_type(T)->optional_type<T>;
+template <typename T>
+optional_type(T)->optional_type<T>;
 
-template <> struct optional_type<> { static constexpr false_t has_value{}; };
+template <>
+struct optional_type<> {
+  static constexpr false_t has_value{};
+};
 
 constexpr optional_type<> nullopt_type;
 
